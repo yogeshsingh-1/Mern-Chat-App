a2ce2548316b9d0685057cdb334a386fcf59ca85
yogeshs <ys730292@gmail.com>
2026-03-20 18:41:39 +0530

    backed

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import "dotenv/config";
-const app = express();
-
-// middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-export default app;
